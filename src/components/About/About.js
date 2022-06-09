4759b750b8ab451abfc26194ff9586dfb1a9883d
KiamKhan <mdlimon0175@gmail.com>
2022-06-09 20:04:47 +0600

    setting.json file deleted

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react'
-import Layout from '../Layout/Layout'
+import React, { Component } from "react";
+import Layout from "../Layout/Layout";
 
 export default class About extends Component {
   render() {
     return (
       <Layout>
-        <section className='py-60 bg-slate-100'>
-        <div className='lg:container mx-auto'>
-          <div>
-            <h2 className='text-4xl font-bold text-gray-600 capitalize mb-4 text-center '>about us</h2>
-          </div>
+        <section className="py-60 bg-slate-100">
+          <div className="lg:container mx-auto">
+            <div>
+              <h2 className="text-4xl font-bold text-gray-600 capitalize mb-4 text-center ">
+                about us
+              </h2>
+            </div>
 
-          <div className='text-center'>
-            <p className='p_text'>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-              <span className='block mt-8'>
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.
-              </span>
-            </p>
+            <div className="text-center">
+              <p className="p_text">
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy.
+                Various versions have evolved over the years, sometimes by
+                accident, sometimes on purpose (injected humour and the like).
+                <span className="block mt-8">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
+                  Many desktop publishing packages and web page editors now use
+                  Lorem Ipsum as their default model text.
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,59 +1,77 @@
-import React, { Component } from 'react'
-import Layout from '../Layout/Layout'
+import React, { Component } from "react";
+import Layout from "../Layout/Layout";
 
 export default class Contact extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            fname: '',
-            lname: '',
-            full_name: ''
-        }
-    }
+    this.state = {
+      fname: "",
+      lname: "",
+      full_name: "",
+    };
+  }
 
-    handleInput = e => {
+  handleInput = (e) => {
+    this.setState((state) => {
+      return {
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
-        this.setState(state =>{
-            return {
-                [e.target.name]: e.target.value
-            }
-        })
-    }
+  submit = (e) => {
+    e.preventDefault();
 
-    submit = e =>{
-        e.preventDefault();
-        
-        this.setState(state =>{
-            return{
-                full_name: state.fname + ' ' + state.lname,
-                fname: '',
-                lname: '',
-                // [e.target.name]: '',
-                // [e.target.value]: '',
-            }
-        })
-    }
+    this.setState((state) => {
+      return {
+        full_name: state.fname + " " + state.lname,
+        fname: "",
+        lname: "",
+        // [e.target.name]: '',
+        // [e.target.value]: '',
+      };
+    });
+  };
   render() {
     return (
       <Layout>
-          <section className='h-screen flex items-center justify-center'>
-            <div className='form_container w-96'>
-                <form onSubmit={this.submit} className="w-full">
-                    <h2 className='text-center common_head'>Contact</h2>
-                    <label className='block text-lg'>Enter your name</label>
-                    <div className='flex mt-8'>
-                        <input name='fname' value={this.state.fname} onChange={this.handleInput} className='mr-4 input' type="text" placeholder='First name'/>
-                        <input name='lname' value={this.state.lname} onChange={this.handleInput}  className='input' type="text" placeholder='Last Name'/>
-                    </div>
+        <section className="h-screen flex items-center justify-center">
+          <div className="form_container w-96">
+            <form onSubmit={this.submit} className="w-full">
+              <h2 className="text-center common_head">Contact</h2>
+              <label className="block text-lg">Enter your name</label>
+              <div className="flex mt-8">
+                <input
+                  name="fname"
+                  value={this.state.fname}
+                  onChange={this.handleInput}
+                  className="mr-4 input"
+                  type="text"
+                  placeholder="First name"
+                />
+                <input
+                  name="lname"
+                  value={this.state.lname}
+                  onChange={this.handleInput}
+                  className="input"
+                  type="text"
+                  placeholder="Last Name"
+                />
+              </div>
 
-                    <button className='btn mx-auto block mt-12'>Submit</button>
-                </form>
+              <button className="btn mx-auto block mt-12">Submit</button>
+            </form>
 
-                <p className='mt-16 text-2xl text-black'>Full name: <span className='text-[#353535] capitalize'>{this.state.full_name}</span></p>
-            </div>
-          </section>
+            <p className="mt-16 text-2xl text-black">
+              Full name:{" "}
+              <span className="text-[#353535] capitalize">
+                {this.state.full_name}
+              </span>
+            </p>
+          </div>
+        </section>
       </Layout>
-    )
+    );
   }
 }
